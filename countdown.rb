f68bf891685c03b31a2a445262bb5d747768834a
Jessica Gladstone <jcg321@comcast.net>
2018-09-25 20:51:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,8 +5,13 @@ def countdown (n)
   while count > 0 
   puts "#{count} SECOND(S)!"
    count -= 1 
+<<<<<<< HEAD
    end
  def countdown_with_sleep(sec)
+=======
+  end
+  def countdown_with_sleep(sec)
+>>>>>>> eb52e23cd42ac33eb734b02f7d8127ae976d6da4
      sleep(sec)
    end
  return "HAPPY NEW YEAR!"
