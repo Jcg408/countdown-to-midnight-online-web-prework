350edf0ba8628be55f5ee3ecb1ca3eb8a97eab4a
Jessica Gladstone <jcg321@comcast.net>
2018-09-25 17:02:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
-
-def countdown
+ 
+def countdown (n)
+  count = n
+  while count > 0 
+  puts "#{count} SECOND(S)!"
+   count -= 1 
+   end
+ def countdown_with_sleep(sec)
+     sleep(sec)
+   end
+ return "HAPPY NEW YEAR!"
 end
